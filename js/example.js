664a3b71ef25bce0222e250b8d284017e92a2c20
toalp <tortor.hadas@gmail.com>
2015-02-22 11:24:47 +0200

    example.js: added enum, fixed code spacing
    added an enum to the json and to the jsonschema. also replaced all the spaces in tabs, for the code to look better.

--- js/example.js
+++ js/example.js
@@ -1,55 +1,60 @@
 "use strict";
 
 var jdata = {
-    "objName": {
-        "title": "this is an object",
+	"objName": {
+		"title": "this is an object",
+		"_id": "234ret56det64",
 		"object2": {
-            "title": "S",
-            "list": [1,2,3,4,5.6],
+			"title": "S",
+			"list": [1,2,3,4,5.6],
+			"myEnum": "option3",
 			"object3": {
-                "objectception": {
-                    "string1": "blah",
+				"objectception": {
+					"string1": "blah",
 					"string2": "blah blah",
 					"string23453": "c2n387rc2",
 					"NotAString": {
-                        "para": "this is where the json does not meet the schema"
-                    }
-                }
-            }
-        }
-    }
+						"para": "this is where the json does not meet the schema"
+					}
+				}
+			}
+		}
+	}
 };
 var jschema = {
-    "type": "object",
-    "properties": {
-        "objName": {
-            "type": "object",
-            "properties": {
-                "title": {type:"string"},
-                "object2": {
-                    "type": "object", 
-                    "properties": {
-                        "title": {"type": "string"},
-                        "list": {
-                            "type": "array",
-                            "items": {
-                                "type": "number"
-                            }
-                        },
-                        "object3": {
-                            "type": "object", 
-                            "properties": {
-                                "objectception": {
-                                    "type": "object", 
-                                    "additionalProperties": {"type": "string"}
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+	"type": "object",
+	"properties": {
+		"objName": {
+			"type": "object",
+			"properties": {
+				"title": {type:"string"},
+				"object2": {
+					"type": "object", 
+					"properties": {
+						"title": {"type": "string"},
+						"list": {
+							"type": "array",
+							"items": {
+								"type": "number"
+							}
+						},
+						"myEnum":{
+							"enum":["option1","option2","option3","option4","LOL"]                        
+						},
+						"object3": {
+							"type": "object", 
+							"properties": {
+								"objectception": {
+									"type": "object", 
+									"additionalProperties": {"type": "string"}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 };
 var child = build($('#data_title'), 'My Data',  $('#data'), jdata, jschema,{edit:true, show:show_object});
 child.toggle();
